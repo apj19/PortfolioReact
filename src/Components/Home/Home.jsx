ef00ac865c5224154890f945521b18e9e9f29da6
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-03-11 11:19:07 +0530

    animation added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "animate.css";
 
 function Home() {
   return (
-    <div className='pl-40'>Home</div>
-  )
+    <div className="px-44 w-full h-full grid grid-cols-2 ">
+      <div className=" ">
+        <h2 className="text-[2rem] animate__animated animate__pulse">
+          Let me help you to ease your{" "}
+          <span className=" text-[#D85C36]">Development</span> Experience
+        </h2>
+        <hr className="border border-[#D85C36] w-[70%]" />
+        <p>
+          Hi Akshay Jadhav (APJCR) here Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Omnis, sapiente.
+        </p>
+      </div>
+      <div className="border border-red-500">1000</div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./hex.css";
 
 function Portfolio() {
   return (
-    <div className='pl-40'>Portfolio</div>
-  )
+    <div className="pl-40 w-full">
+      <div className="flex items-center justify-evenly">
+        <hr className="w-[38%] animate__animated animate__fadeInLeft" />
+        <h2 className="tracking-widest animate__animated animate__fadeInDown">
+          PORTFOLIO
+        </h2>
+        <hr className="w-[38%] animate__animated animate__fadeInRight" />
+      </div>
+
+      {/* hexagone */}
+      <div class="hexagon"></div>
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
