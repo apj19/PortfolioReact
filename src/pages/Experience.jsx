6d586699cdfb860dd08a0bf7fd36267cd3984412
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-04-22 08:25:49 +0530

    working on resume

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Experience() {
-  return <main className="min-h-[85vh]">Experience</main>;
+  return (
+    <main className="min-h-[85vh]">
+      Experience worked in ppl tool worked in merge issue Page is under
+      construction
+    </main>
+  );
 }
 
 export default Experience;
