ac0e43c25b4823a51d9a849c13e1024ed1cbd77e
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-04-24 06:55:38 +0530

    resume

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -5,7 +5,9 @@ function Experience() {
     <main className="min-h-[85vh]">
       Experience worked in ppl tool worked in merge issue Page is under
       construction woked with merge team to resolve issue manually and then
-      build UI to simplify the merge process.
+      build UI to simplify the merge process. Worked with database team to
+      understand the resouation process for merge incidents then accounrding to
+      requries build the
     </main>
   );
 }
