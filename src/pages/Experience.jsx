cff34e52fdcb4737b983d4409c485819047c61ba
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-04-25 17:28:00 +0530

    working on Resume

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -5,19 +5,50 @@ function Experience() {
     <>
       <section className="flex justify-center mt-10">
         <section className=" flex flex-col   h-[100vh]  w-[60%]">
-          <div>
-            Experience worked in ppl tool worked in merge issue Page is under
-            construction woked with merge team to resolve issue manually and
-            then build UI to simplify the merge process. Worked with database
-            team to understand the resouation process for merge incidents then
-            accounrding to requries build the
+          <section className="flex justify-between pb-4 border-b ">
+            <div className="flex flex-col justify-start">
+              <h2 className="text-[1.6rem] mb-4">Akshay Pandurang Jadhav</h2>
+              <p className="hover:text-pink-500 mb-2">
+                <i className="fa-regular fa-envelope text-pink-500 mr-4"></i>
+                akshayjadhav2133@gmail.com
+              </p>
+              <p className="hover:text-pink-500">
+                <i className="fa-solid fa-phone text-pink-500 mr-4"></i>
+                9730187072
+              </p>
+            </div>
+            <div className="w-[30%]">
+              <img className="w-40 h-40" src="../APJCR2.png" alt="" />
+            </div>
+          </section>
+          <div className="my-4">
+            <h3 className="text-blue-500 text-[1.2rem]">Career Objective</h3>
+            <p>
+              Seeking a position that will improve my expertise in React and
+              related technologies, while contributing to the development of
+              innovative and impactful web solutions.
+            </p>
+            <ul className="pl-5">
+              <li className="text-blue-200 font-bold">Frontend</li>
+              <li className="text-blue-200 font-bold">Database</li>
+              <li className="text-blue-200 font-bold">Other Technologies</li>
+            </ul>
           </div>
-          <div>
-            Experience worked in ppl tool worked in merge issue Page is under
-            construction woked with merge team to resolve issue manually and
-            then build UI to simplify the merge process. Worked with database
-            team to understand the resouation process for merge incidents then
-            accounrding to requries build the
+          <div className="my-4">
+            <h3 className="text-blue-500 text-[1.2rem]">Key Skills</h3>
+          </div>
+          <div className="my-4">
+            <h3 className="text-blue-500 text-[1.2rem]">Experience</h3>
+          </div>
+          <div className="my-4">
+            <h3 className="text-blue-500 text-[1.2rem]">
+              Personal Development
+            </h3>
+          </div>
+          <div className="my-4">
+            <h3 className="text-blue-500 text-[1.2rem]">
+              Educational Qualification
+            </h3>
           </div>
         </section>
       </section>
