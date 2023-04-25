8bb1a44b76f46818350177e8fd043647f1c15f50
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-04-25 07:26:03 +0530

    resume format

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -2,13 +2,26 @@ import React from "react";
 
 function Experience() {
   return (
-    <main className="min-h-[85vh]">
-      Experience worked in ppl tool worked in merge issue Page is under
-      construction woked with merge team to resolve issue manually and then
-      build UI to simplify the merge process. Worked with database team to
-      understand the resouation process for merge incidents then accounrding to
-      requries build the
-    </main>
+    <>
+      <section className="flex justify-center mt-10">
+        <section className=" flex flex-col   h-[100vh]  w-[60%]">
+          <div>
+            Experience worked in ppl tool worked in merge issue Page is under
+            construction woked with merge team to resolve issue manually and
+            then build UI to simplify the merge process. Worked with database
+            team to understand the resouation process for merge incidents then
+            accounrding to requries build the
+          </div>
+          <div>
+            Experience worked in ppl tool worked in merge issue Page is under
+            construction woked with merge team to resolve issue manually and
+            then build UI to simplify the merge process. Worked with database
+            team to understand the resouation process for merge incidents then
+            accounrding to requries build the
+          </div>
+        </section>
+      </section>
+    </>
   );
 }
 
